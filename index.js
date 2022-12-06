2501fa028a93a4ec5673bb3f1ff78d9b437d51c3
kevinkkimutai <kiruikevinkim@gmail.com>
2022-12-06 17:43:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+function submitData(name, email) {
+    let users = {
+        name: name,
+        email: email
+    };
+
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(users)
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(object) {
+            let h2 = document.createElement('h2');
+            h2.innerHTML = object.id;
+            document.body.appendChild(h2);
+            
+        })
+        .catch(function(error) {
+            let h2 = document.createElement('h2');
+            h2.innerHTML = error.message;
+            document.body.appendChild(h2);
+        });
+}
+submitData('kelvin', 'kimutai');
+
